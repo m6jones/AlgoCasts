00e6f4ab9bff4c3a2e28743078452c9049f1b31d
Maya <maya@circleci.com>
2022-03-02 10:08:00 -0500

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,11 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    for(let i = 0, j = str.length - 1; i < j; i++, j--) {
+        if (str[i] !== str[j]) return false;
+    }
+    return true;
+}
 
 module.exports = palindrome;
