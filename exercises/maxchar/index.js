0704ce6f8d4a40e38c8df50aeefd1bba8e82ef09
Maya <maya@circleci.com>
2022-03-02 11:29:54 -0500

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let charCount = {};
+    for (const char of str) {
+        if (!charCount[char]) {
+            charCount[char] = 0;
+        }
+        charCount[char]++;
+    }
+
+    return Object.keys(charCount).reduce((reducer, char) => {
+        if (reducer.count < charCount[char]) {
+            return {char, count: charCount[char]}
+        }
+        return reducer;
+    }, {char: '', count: 0}).char;
+}
 
 module.exports = maxChar;
